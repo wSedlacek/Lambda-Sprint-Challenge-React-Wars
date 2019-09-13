d0e76a9b98e03bda6b733a323e875bfd4ebd6ab0
William Sedlacek <wsedlacekc@gmail.com>
2019-09-13 11:15:58 -0500

    Style Components

--- starwars/src/App.tsx
+++ starwars/src/App.tsx
@@ -1,13 +1,29 @@
-import './App.scss';
 import React from 'react';
 import { useEffect, useState } from 'react';
 
 import axios from 'axios';
+import styled from 'styled-components';
 
 import { Person } from './components/person/person.component';
 
 import { Character } from './models/Character';
 import { Characters } from './models/Characters';
+
+const Root = styled.div`
+  text-align: center;
+`;
+
+const Header = styled.h1`
+  color: #443e3e;
+  text-shadow: 1px 1px 5px #fff;
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: 1fr 1fr 1fr;
+  margin: 10px;
+`;
 
 export const App = () => {
   const [people, setPeople] = useState<Character[]>([]);
@@ -23,11 +39,13 @@ export const App = () => {
   useEffect(() => getPeople('https://swapi.co/api/people/?page=1'), []);
 
   return (
-    <div className='App'>
-      <h1 className='Header'>React Wars</h1>
-      {people.map(person => (
-        <Person character={person} />
-      ))}
-    </div>
+    <Root>
+      <Header>React Wars</Header>
+      <Grid>
+        {people.map(person => (
+          <Person character={person} />
+        ))}
+      </Grid>
+    </Root>
   );
 };
--- starwars/src/components/person/person.component.tsx
+++ starwars/src/components/person/person.component.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
 import { Character } from '../../models/Character';
 
+const Animate = styled.div`
+  animation: fadein 0.5s forwards;
+`;
+
 export const Person = ({ character }: { character: Character }) => {
-  return <div>{character.name}</div>;
+  return (
+    <Animate>
+      <Card>
+        <CardContent>
+          <Typography gutterBottom variant='h5' component='h2'>
+            {character.name}
+          </Typography>
+          <Typography variant='body2' color='textSecondary' component='p'>
+            Height: {character.height} cm
+          </Typography>
+          <Typography variant='body2' color='textSecondary' component='p'>
+            Mass: {character.mass} kg
+          </Typography>
+          <Typography variant='body2' color='textSecondary' component='p'>
+            Eye Color: {character.eye_color}
+          </Typography>
+          <Typography variant='body2' color='textSecondary' component='p'>
+            Hair Color: {character.hair_color}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Animate>
+  );
 };
