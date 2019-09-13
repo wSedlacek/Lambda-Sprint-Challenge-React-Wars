c56de5a8f5e6d1545bff2e330e26756baec33f56
William Sedlacek <wsedlacekc@gmail.com>
2019-09-13 10:29:33 -0500

    Setup SCSS

--- starwars/src/App.tsx
+++ starwars/src/App.tsx
@@ -1,5 +1,5 @@
+import './App.scss';
 import React from 'react';
-import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -10,10 +10,10 @@ const App = () => {
   // sync up with, if any.
 
   return (
-    <div className="App">
-      <h1 className="Header">React Wars</h1>
+    <div className='App'>
+      <h1 className='Header'>React Wars</h1>
     </div>
   );
-}
+};
 
 export default App;
